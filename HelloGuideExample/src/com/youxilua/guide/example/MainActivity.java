ecf8ee012e6d8fd918e133d613d0ae84fbd1cbd5
renyuan <renyuan@minephone.com>
2012-09-10 00:44:13 +0800

    update

--- HelloGuideExample/src/com/youxilua/guide/example/MainActivity.java
+++ HelloGuideExample/src/com/youxilua/guide/example/MainActivity.java
@@ -9,13 +9,12 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
